4db5b6448dee3f4c896c70619f43c43bc2ee7426
basan <basanton@mail.ru>
2024-06-05 16:27:11 +0300

    +Valute

--- src/main/java/ru/Balakhashvili/ExchangeRateTopBot/exception/ServiceException.java
+++
@@ -1,8 +0,0 @@
-package ru.Balakhashvili.ExchangeRateTopBot.exception;
-
-public class ServiceException extends Exception {
-
-    public ServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
